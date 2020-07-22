5ecb8861a172ce11695b525b3a661b8fa4532a56
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-22 06:23:03 -0400

    documentation

--- driver.py
+++ driver.py
@@ -1,5 +1,6 @@
 from controller_keyboard import control_keyboard
 from controller_mouse import control_mouse
+# Note: pynput cannot control the keyboard and mouse within the same file, this requires double imports
 
 # Program stores key strokes and mouse movement within log file
 
