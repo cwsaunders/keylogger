31e2c15297c08c499bb4143b96fb5c8ce89a4219
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-22 06:05:28 -0400

    gg

---
+++ controller.py
@@ -0,0 +1,1 @@
+from pynput.mouse import Controller
--- driver.py
+++ driver.py
@@ -1,10 +1,6 @@
 # Program stores key strokes and mouse movement within log file
 
 # Appending
-log_w = open('keylogger/log.txt','a')
-log_w.write('')
-log_w.close()
-
-
-
+with open('keylogger/log.txt','a') as f:
+    f.write('')
 
